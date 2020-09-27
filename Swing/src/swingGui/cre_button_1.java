913b82b634ecc5d392df5888f58f1820d21a27b3
engineerscodes <practicalengineers05@gmail.com>
2020-09-27 14:57:26 +0530

    Swing demo1

---
+++ Swing/src/swingGui/cre_button_1.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package swingGui;
+import javax.swing.*;
+/**
+ * @author M.NAVEEN
+   RANDOM CODER'S
+ *
+ */
+public class cre_button_1 
+{
+    JButton b1;
+    JFrame f;
+	public cre_button_1() 
+	{  f=new JFrame();
+       b1=new JButton ();  
+	}
+
+	public void build() 
+	{
+		b1.setBounds(150,150,150,50);
+		f.add(b1);
+		b1.setText("hello");
+		b1.setText("Button");
+		f.setTitle("RANDOM CODER'S");
+		f.setSize(500,500);
+		f.setLayout(null);
+		f.setVisible(true);
+	}
+	public static void main(String[] args)
+	{
+         new cre_button_1().build();
+         
+	}
+
+}
