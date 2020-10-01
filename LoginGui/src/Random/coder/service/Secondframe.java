b80d8bc90d434cccecab4a2e80b669c0228af3c7
engineerscodes <practicalengineers05@gmail.com>
2020-10-01 16:25:13 +0530

    Second frame

---
+++ LoginGui/src/Random/coder/service/Secondframe.java
@@ -0,0 +1,55 @@
+/**
+ * 
+ */
+package Random.coder.service;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+
+/**
+ * @author M.NAVEEN
+   RANDOM CODER'S
+ *
+ */
+public class Secondframe extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Secondframe frame = new Secondframe();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Secondframe() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setBounds(405, 0, 27, 25);
+		contentPane.add(btnNewButton);
+	}
+
+}
