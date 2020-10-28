141d0f26419ea761c5d981b552eb32129f683f8f
engineerscodes <practicalengineers05@gmail.com>
2020-10-28 12:41:20 +0530

    kcryto

---
+++ Swing/src/swingGui/modcal.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package swingGui;
+
+/**
+ *  @author M.NAVEEN
+ *  RANDOM CODER'S
+ *  Tech/Project Lead Android Club
+ */
+public class modcal {
+
+public long calmod(long n1,long n2)
+{return n1%n2;
+}
+}
+
